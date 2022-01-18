aa4dd0cadb7304dd8f68b791c1712b29967419e2
Héctor Martínez <hector.martinez@the-cocktail.com>
2022-01-19 00:06:52 +0100

    firebase anayltics

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
+import { getAnalytics, logEvent } from "firebase/analytics";// TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
@@ -19,5 +18,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+logEvent(analytics, 'page_view', `your_event_parameter`)
 
-export { app, analytics };
+
